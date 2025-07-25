8574919acbb3aa50d7f0cfae6ad540855d644277
Dav2dAnderson <dav2danderson@gmail.com>
2025-07-25 22:55:23 +0500

    edition: serializers

--- courses/serializers.py
+++ courses/serializers.py
@@ -8,27 +8,6 @@ class CourseSerializer(serializers.ModelSerializer):
         model = Course
         fields = ['id', 'name', 'slug', 'price', 'image']
 
-    
-class ModuleSerializer(serializers.ModelSerializer):
-    course = serializers.CharField(source='course.name', read_only=True)
-    class Meta:
-        model = Modules
-        fields = ['name', 'course', 'file']
-
-
-class CourseDetailSerializer(serializers.ModelSerializer):
-    teacher = serializers.CharField(source='teacher.username', read_only=True)
-    modules = ModuleSerializer(many=True, read_only=True)
-    class Meta:
-        model = Course
-        fields = ['id', 'name', 'slug', 'description', 'price', 'created_at', 'image', 'teacher_id', 'teacher', 'modules']
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = ['title', 'slug', 'module']
-
 
 class LessonDetailSerializer(serializers.ModelSerializer):
     lesson_files = serializers.SerializerMethodField()
@@ -43,9 +22,40 @@ class LessonDetailSerializer(serializers.ModelSerializer):
         if course.students.filter(id=user.id).exists():
             return LessonFilesSerializer(obj.lesson_files.all(), many=True).data
         return []
-    
+
 
 class LessonFilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = LessonFile
         fields = ['id', 'file', 'lesson']
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    # lesson_files = LessonFilesSerializer(many=True, read_only=True)
+    class Meta:
+        model = Lesson
+        fields = ['title', 'slug', 'module', 'lesson_files']
+
+    def get_lesson_files(self, obj):
+        user = self.context['request'].user
+        course = obj.module.course
+        if course.students.filter(id=user.id).exists():
+            return LessonFilesSerializer(obj.lesson_files.all(), many=True).data
+        return []
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    course = serializers.CharField(source='course.name', read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
+    class Meta:
+        model = Modules
+        fields = ['name', 'course', 'file', 'lessons']
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    teacher = serializers.CharField(source='teacher.username', read_only=True)
+    modules = ModuleSerializer(many=True, read_only=True)
+    class Meta:
+        model = Course
+        fields = ['id', 'name', 'slug', 'description', 'price', 'created_at', 'image', 'teacher_id', 'teacher', 'modules']
+
